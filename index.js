06a2c345219c2387ffc2c8403242f527f8f744ce
tresoraban <ntungane.tresor@student.com>
2025-03-11 04:20:44 -0700

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// 1) is an equation whose multiplied results will equal 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// 2) generates a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// 3) is an equation that calculates a remainder that is equal to 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// 4) will return 20 as the highest number in the set
+const max = Math.max(1, 2, 3, 4, 20);
